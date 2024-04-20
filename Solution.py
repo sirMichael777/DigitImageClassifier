c6a4a5346b418f3be14e69c456208776dcd7c1bf
Michael Maseko <mskmic017@myuct.ac.za>
2024-04-20 11:20:13 +0000

    Neural network defined properly

--- Solution.py
+++ Solution.py
@@ -1,1 +1,41 @@
-print("hi")
+import torch
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+from torch import nn, optim
+import sys
+from PIL import Image
+from pathlib import Path
+
+# Data preprocessing
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5,), (0.5,))])
+
+# Load MNIST data/home/m/mskmic017/Desktop/CSC3022F/mlAssignment1/MNIST_JPGS/testSample/testSample
+train_set = MNIST(root='./data', train=True, download=True, transform=transform)
+test_set = MNIST(root='./data', train=False, download=True, transform=transform)
+
+train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
+
+# Neural network definition
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28*28, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 64)
+        self.fc4 = nn.Linear(64, 10)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.dropout(self.relu(self.fc2(x)))
+        x = self.dropout(self.relu(self.fc3(x)))
+        x = self.fc4(x)
+        return x
+
+model = Net()
+
